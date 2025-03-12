d29d75287676b7ac62686ea94a0657235ec4286d
Ayodeji <dejiowoeye@gmail.com>
2025-03-12 13:41:29 +0100

    Modified the calculator and guessing game

--- calculator.py
+++ calculator.py
@@ -1,14 +1,19 @@
-import pyinputplus as pyip
-num1 = pyip.inputNum(prompt := "First Number: ")
-operator = input("operator : ")
-num2 = pyip.inputNum(prompt := "Second Number: ")
-if operator == "+": 
-    print(num1 + num2)
-elif operator == "-":
-    print(num1 -num2)
-elif operator == "*":
-    print(num1 * num2)
-elif operator == "/":
-    print(num1/num2)
-else: 
-    print("check operator")
+"""Here is a claculator that can perform any of addition, subtraction, multiplication or division"""
+try:
+    Calculate = input("Display input: ")
+    num = Calculate.split(" ")
+    num[0] = int(num[0])
+    num[2] = int(num[2])
+    if num[1] == "+": 
+        print(num[0] + num[2])
+    elif num[1] == "-":
+        print(num[0] -num[2])
+    elif num[1] == "*":
+        print(num[0] * num[2])
+    elif num[1] == "/":
+        print(num[0]/num[2])
+    else: 
+        print("check operator")
+except: 
+    print("invalid input, correct format: A + B")
+print(num)
--- guessing_game.py
+++ guessing_game.py
@@ -1,33 +1,36 @@
 import pyinputplus as pyin
 import random as rn 
-n = rn.randint(0,15)
-guess = pyin.inputInt(prompt := "make a guess: ")
+n = rn.randint(0,15) # generating random numbers for the guessing game system 
+"""
+We will need to set a count for the life 
+a list to insert the guess for checks 
+and a count for the guess made by the perticipant 
+"""
 life_count = 3
 guess_list = []
-guess = int(guess)
 guess_count = 0
-while (guess_count < 5) and (guess != n) and life_count > 0:
-        
+while (guess_count < 5) and life_count > 0:  
+    """This loop keeps the game running as long 
+    as the life count is higher than zero and 
+    participants made less than 5 guesses""" 
+    guess = int(pyin.inputInt(prompt := "make a guess: ") )
     if (guess > n) and (guess in guess_list):
         print("guess is too high")
     elif (guess > n) and (guess not in guess_list):
         life_count -= 1
-        print("guess is too high and you loss a live")
-        print(f"Your Live count is {life_count}")
+        print(f"guess is too high and your life count is reduced to {life_count}")
     elif (guess < n) and (guess in guess_list):
         print("guess is too low")
-    elif guess < n and guess not in guess_list:
+    elif (guess < n) and (guess not in guess_list):
         life_count -= 1
-        print("guess is too low and you loss a live")
-        print(f"Your Live count is {life_count}")
+        print(f"guess is too low and your life count is reduced to {life_count}")
     elif guess == n:
         print("you are correct")
         break
     guess_count += 1
-        
     guess_list.append(guess)
-    guess = pyin.inputInt(prompt:= "make a guess: ")
-print(f"the correct number is {n}")
+    
+print(f"the correct number is {n}") #important we let participants know our selected number.
         
      
      
