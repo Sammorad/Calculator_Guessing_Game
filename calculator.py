42fd8713e19c14f450881c17f6473aa2b4827403
Ayodeji <dejiowoeye@gmail.com>
2025-03-23 14:47:29 +0100

    used functions to break code in units

--- calculator.py
+++ calculator.py
@@ -1,18 +1,33 @@
-"""Here is a claculator that can perform any of addition, subtraction, multiplication or division"""
-try:
-    Calculate = input("Display input: ")
-    num = Calculate.split(" ")
-    num[0] = int(num[0])
-    num[2] = int(num[2])
-    if num[1] == "+": 
-        print(num[0] + num[2])
-    elif num[1] == "-":
-        print(num[0] -num[2])
-    elif num[1] == "*":
-        print(num[0] * num[2])
-    elif num[1] == "/":
-        print(num[0]/num[2])
-    else: 
-        print("check operator")
-except: 
-    print("invalid input, correct format: A + B")
+def calculator():
+    """Here is a claculator that can perform any of addition, subtraction, multiplication or division"""
+    while True:
+        details = input("Type 'exit' to quit or input details: ")
+        if details == "exit":
+            break
+        """lets take care of invalid inputs"""
+        try: 
+            num = details.split(" ")
+            num[0] = int(num[0])
+            num[2] = int(num[2])
+
+        except:
+            print("invalid input correct format : A + B")
+        else:
+            
+            if num[1] == "+": 
+                print(num[0] + num[2])
+            elif num[1] == "-":
+                print(num[0] -num[2])
+            elif num[1] == "*":
+                print(num[0] * num[2])
+            elif num[1] == "/":
+                """lets take care of zerp division error"""
+                try:
+                    print(num[0]/num[2])
+                except ZeroDivisionError:
+                    print("You cannot divide by zero")
+                else:
+                    print(num[0]/num[2])
+            
+calculator()  
+
--- guessing_game.py
+++ guessing_game.py
@@ -1,35 +1,58 @@
-import pyinputplus as pyin
 import random as rn 
-n = rn.randint(0,15) # generating random numbers for the guessing game system 
-"""
-We will need to set a count for the life 
-a list to insert the guess for checks 
-and a count for the guess made by the perticipant 
-"""
-guess_count = 0
-life_count = 3
-guess_list = []
-while True: 
-    guess = int(pyin.inputInt(prompt := "make a guess: ") )
-    if guess_count > 5 or life_count == 0:
-        print(f"game over. The correct number is {n}")
-        break
-    elif (guess > n) and (guess in guess_list):
-        print("guess is too high")
-    elif (guess > n) and (guess not in guess_list):
-        life_count -= 1
-        print(f"guess is too high and your life count is reduced to {life_count}")
-    elif (guess < n) and (guess in guess_list):
-        print("guess is too low")
-    elif (guess < n) and (guess not in guess_list):
-        life_count -= 1
-        print(f"guess is too low and your life count is reduced to {life_count}")
-    elif guess == n:
-        print("you are correct")
-        break
-    guess_count += 1
-    guess_list.append(guess)
+def get_user_guess():
+    guess = int(input("Enter your guess: "))
+    return guess
+
+def play_game():
+    """the main guessing game,
+    It takes input and test the guessing game"""
+    live_count = 3
+    guessing_list = []
+    no_of_guesses  = 0
+    number = rn.randint(1,15)
+    print("Welcome to your Guessing game\n")
+    print(f"You have {live_count} lives")
+    while live_count > 0:
+        guess = get_user_guess()
+        no_of_guesses += 1
+        if guess == number:
+            print(f"your guess is correct")
+            break
+
+        elif guess > number and guess not in guessing_list:
+            print("Your guess is too high")
+            guessing_list.append(guess)
+            live_count -= 1
+        elif guess > number and guess in guessing_list:
+            print("Your guess is too high")
+
+        elif guess < number and guess not in guessing_list :
+            print("your guess is too low")
+            guessing_list.append(guess)
+            live_count -= 1
+
+        elif guess < number and guess in guessing_list:
+            print("your guess is too low ")
+    else:
+        print(f"Game over, correct number is {number}")
+def main():
+    """This loop will ensure the game 
+    continues in an interactive manner """
     
-        
-     
-     
+    while True:
+        play_game()
+        play_again = input("Will you play another game? (yes/no)").strip().lower()
+        if play_again == "no":
+            print("Its great having you play the game, goodbye")
+            break
+        elif play_again == "yes":
+            continue
+
+main()
+
+
+
+
+
+
+    
